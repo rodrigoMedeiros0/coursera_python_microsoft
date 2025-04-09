5c7a3e3be96aaf6f5fc4803999d8878abb32feee
Rodrigo Medeiros <rodrigo.medeiros@g3sa.com.br>
2025-04-09 15:48:39 -0300

    primeiros programas

---
+++ python_fundamentos/modulo_1/operacoes_matematicas.py
@@ -0,0 +1,41 @@
+#Adição + 
+#Subtração - 
+#Divisão /
+#Multiplicação *
+#Exponenciação **
+#Divisão inteira //
+#Módulo %
+
+
+#Exercicio soma 
+a = 5
+b = 7
+soma = a + b
+
+print(f"A soma de {a} e {b} é {soma}")
+
+#Exericio subtração
+x = 20 
+y = 8
+subtracao = x - y
+print(f"A subtração de {x} e {y} é {subtracao}")
+
+#Exercicio multiplicação
+resultado_multiplicação = 4 * 6
+print(f"A multiplicação de 4 e 6 é {resultado_multiplicação}")
+
+#Exercicio divisão
+resultado_divisão = 15 / 3
+print(f"A divisão de 15 e 3 é {resultado_divisão}")
+
+#Exercicio divisão inteira
+resultado_divisão_inteira = 17 // 5
+print(f"A divisão inteira de 17 e 5 é {resultado_divisão_inteira}")
+
+#Exercicio módulo
+resto_divisao = 29 % 4
+print(f"O resto da divisão de 29 por 4 é {resto_divisao}")
+
+#Exercicio exponenciação
+resultado_exponenciacao = 3 ** 4
+print(f"O resultado de 3 elevado a 4 é {resultado_exponenciacao}")
